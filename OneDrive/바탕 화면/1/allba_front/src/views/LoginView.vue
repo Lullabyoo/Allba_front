d5593d9ea19792b95666b59a58722ab9305ffd2d
유영서 <youngseo1314@sookmyung.ac.kr>
2023-01-08 17:02:24 +0900

    로그인 추가

--- OneDrive/바탕 화면/1/allba_front/src/views/LoginView.vue
+++ OneDrive/바탕 화면/1/allba_front/src/views/LoginView.vue
@@ -1,6 +1,19 @@
 <template>
     <div>
-        login
+        <body>
+        <div class="main">
+        <h1 class="logo">Login</h1>
+        <div class="container">
+            <input type="text" placeholder="ID" id="id" class="account">
+            <input type="password" placeholder="Password" id="password" class="account">
+            <button id="login" class="account">login</button>
+            <p id="alert" class="account"> </p>
+            
+        </div>
+    </div>   
+    <link rel="stylesheet" href="css/login.css">
+</body>
+
     </div>
 </template>
 <script>
@@ -9,5 +22,41 @@ export default {
 }
 </script>
 <style>
+.main {
+    width: 1260px;
+    height: 580px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    border: 1px solid lightgrey;
+    border-radius: 5px;
+}
+
+.logo {
+        margin-top: 0px;
+        margin-bottom: 40px;
+}
+
+
+#login {
+    width: 100%;
+    background-color: skyblue;
+    border-color: transparent;
+    color: white;
+}
+
+.account {
+    display: block;
+    margin-bottom: 3px;
+    padding: 3px;
+    border: 1px solid lightgray;
+    border-radius: 3px;
+}
+
+#alert {
+    border-color: transparent;
+}
+
     
 </style>
